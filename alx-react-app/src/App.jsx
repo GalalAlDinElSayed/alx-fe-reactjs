c8e03e40fa301eb952d89d0df05cc73f19d50a8e
GalalAlDinElSayed <googoband@gmail.com>
2025-02-17 19:11:48 +0200

    Implement Header, MainContent, Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Header from 'components/Header';
+import MainContent from 'MainContent';
+import Footer from 'Footer';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
