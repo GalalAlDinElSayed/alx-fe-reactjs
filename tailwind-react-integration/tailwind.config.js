63e258d254083074ba0bdc66774242c2a059196c
GalalAlDinElSayed <googoband@gmail.com>
2025-04-02 18:03:31 +0200

    Initial commit with Tailwind setup

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], 
+    darkMode: false, 
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
